9da34b980f139bb1c633e1777aba83b31aeaeec3
victor.telles@iteso.mx <victor.telles@iteso.mx>
2025-03-18 19:05:31 -0600

    Fix pre-commit issues
    pre-commit issues and add ")"

--- tests/test_white_box.py
+++ tests/test_white_box.py
@@ -42,6 +42,7 @@ from src.white_box import (
     validate_password,
     validate_url,
     verify_age,
+)
 
 # from src.white_box import VendingMachine, divide, get_grade, is_even, is_triangle
 
@@ -157,6 +158,7 @@ class TestWhiteBoxTriangle(unittest.TestCase):
         """Checks behaivor when one side is zero"""
         self.assertEqual(is_triangle(0, 4, 5), "No, it's not a triangle.")
 
+
 class TestWhiteBoxCheckNum(unittest.TestCase):
     """White-box unittest class."""
 
@@ -184,6 +186,7 @@ class TestWhiteBoxCheckNum(unittest.TestCase):
 
 class TestWhiteBoxValidatePass(unittest.TestCase):
     """White-box unittest class - #2 Validate_password."""
+
     # Test cases 2 = "validate_password(password)"
     def test_validate_password_valid(self):
         """Checks if a valid password thats meets all requerimetns returns True."""
@@ -220,6 +223,7 @@ class TestWhiteBoxValidatePass(unittest.TestCase):
     def test_validate_password_with_invalid_special_char(self):
         """Checks if a password with special characters not in the required set returns False."""
         self.assertFalse(validate_password("Password1*"))
+
 
 class TestWhiteBoxCalculatorTotalDiscount(unittest.TestCase):
     """White-box unittest class - #3 Calculator_total_discount."""
@@ -826,6 +830,7 @@ class TestWhiteBoxGetWeatherAdvisory(unittest.TestCase):
     def test_get_weather_advisory_with_no_specific_condition(self):
         """Check weather advisory for normal conditions."""
         self.assertEqual(get_weather_advisory(20, 50), "No Specific Advisory")
+
 
 class TestWhiteBoxVendingMachine(unittest.TestCase):
     """White-box unittest class - class for Vending Machine."""
